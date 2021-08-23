9ba655c47dda378e43b65a9b40bfe58d8f19aad1
mohamadalvir <mohamadalvin@gmail.com>
2021-08-23 18:50:30 +0700

    alvilab.tech

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,119 @@
-import Head from 'next/head'
+import Head from "next/head";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const [typingIndicator, setTypingIndicator] = useState(true);
+  const [bubbles, setbubbles] = useState([
+    { txt: "Halo 👋", show: false, delay: 500 },
+    { txt: "Kenalin, gue Alvira Mohamad", show: false, delay: 1500 },
+    {
+      txt: "Saat ini, gue lagi merintis karir sebagai seorang programmer.",
+      show: false,
+      delay: 4000,
+    },
+    {
+      txt: "Khususnya di bidang web frontend developer.",
+      show: false,
+      delay: 6000,
+    },
+    {
+      txt: "Web ini lagi dibenerin ya .",
+      show: false,
+      delay: 8000,
+    },
+    {
+      txt: "Btw, jaga kesehatan selalu! 😷",
+      show: false,
+      delay: 12000,
+    },
+  ]);
+
+  const timeline = (count) => {
+    setTimeout(() => {
+      if (count < bubbles.length) {
+        let items = [...bubbles];
+        items[count].show = true;
+        setbubbles(items);
+      }
+    }, bubbles[count].delay);
+  };
+
+  useEffect(() => {
+    let calcTypingSpeed = 0;
+    bubbles.forEach((element, key) => {
+      timeline(key);
+      if (calcTypingSpeed < element.delay) calcTypingSpeed = element.delay;
+    });
+
+    setTimeout(() => {
+      setTypingIndicator(false);
+    }, calcTypingSpeed);
+  }, []);
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="flex flex-col justify-center min-h-screen py-2">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <h1 className="font-serif text-4xl text-left px-6 md:px-36 mt-6">
+        alvilab.tech
+      </h1>
+      <div className="flex flex-col justify-end flex-1 px-6 md:px-36 text-center mb-6">
+        <div className="flex flex-col items-start justify-around">
+          {bubbles.map((item, key) => {
+            return (
+              <p
+                key={key}
+                className={`${
+                  item.show ? "animate-bounce-bottom-left" : "hidden"
+                } px-6 py-2 mt-4 text-left border rounded-xl ml-2 shadow-sm `}
+              >
+                {item.txt}
+              </p>
+            );
+          })}
+          <div className="flex items-center">
+            <div className="w-14 h-14 rounded-full  mt-4 overflow-hidden shadow-lg border-white border-2">
+              <img src="./me.jpg" />
+            </div>
+            <div className={`mx-2 mt-3 ${typingIndicator ? "" : "hidden"}`}>
+              <svg
+                id="prefix__typing_bubble"
+                data-name="typing bubble"
+                xmlns="http://www.w3.org/2000/svg"
+                width={24}
+                height={6}
+                viewBox="0 0 24 6"
+              >
+                <defs>
+                  <style>
+                    {
+                      "@keyframes ball-beat{0%{opacity:.7}33.33%{opacity:.55}66.67%{opacity:.4}to{opacity:1}}.prefix__dot{fill:rgb(24 37 78 / 70%);transform-origin:50% 50%;animation:ball-beat 1.1s 0s infinite cubic-bezier(.445,.05,.55,.95)}.prefix__dot:nth-child(2){animation-delay:.3s!important}.prefix__dot:nth-child(3){animation-delay:.6s!important}"
+                    }
+                  </style>
+                </defs>
+                <circle className="prefix__dot" cx={3} cy={3} r={3} />
+                <circle className="prefix__dot" cx={12} cy={3} r={3} />
+                <circle className="prefix__dot" cx={21} cy={3} r={3} />
+              </svg>
+            </div>
+          </div>
         </div>
-      </main>
+      </div>
 
       <footer className="flex items-center justify-center w-full h-24 border-t">
         <a
-          className="flex items-center justify-center"
+          className={"flex items-center justify-center "}
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
         </a>
       </footer>
     </div>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,31 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "bounce-bottom-left": "bounce-bottom-left 1s",
+      },
+      keyframes: {
+        "bounce-bottom-left": {
+          "0%": {
+            opacity: 0.1,
+            margin: 0,
+            transform: "translate(-50%, 50%) scale(0.4)",
+            "animation-timing-function": "cubic-bezier(0.18, 0.89, 0.32, 1.2)",
+          },
+          "50%": {
+            opacity: 1,
+            transform: "translate(0) scale(1)",
+            "animation-timing-function": "cubic-bezier(0.8, 0, 1, 1)",
+          },
+        },
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
