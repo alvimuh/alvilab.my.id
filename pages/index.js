4afa1bc395f4115006d465ef5b13c20e7d1b8cf5
mohamadalvir <mohamadalvin@gmail.com>
2021-08-23 22:26:03 +0700

    Natural typing

--- pages/index.js
+++ pages/index.js
@@ -37,7 +37,7 @@ export default function Home() {
         setTypingIndicator(false);
         setTimeout(() => {
           setTypingIndicator(true);
-        }, 500);
+        }, 800);
       }
     }, bubbles[count].delay);
   };
