0cab25a9cfd935d8347809ff4abf9080d583a06c
alvimuh <alvira@code.id>
2023-05-06 22:41:27 +0700

    Update footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,37 +3,53 @@ import Button from "./Button";
 const Footer = () => {
   return (
     <footer className="container-default py-6 flex flex-col md:flex-row justify-between gap-4 md:items-center">
-      <div className="flex flex-col md:flex-row gap-1 md:gap-3">
+      <div className="flex flex-col md:flex-row gap-1 md:gap-3 md:items-center">
         <div className="flex items-center gap-4">
           <span class="relative flex h-3 w-3">
             <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-600 opacity-75"></span>
             <span class="relative inline-flex rounded-full h-3 w-3 bg-green-600"></span>
           </span>
-          <h5 className="text-xl mb-[2px]">Keep in touch</h5>
-          <p className="text-gray-700">Follow on</p>
+          <h5 className="text-xl mb-[2px] font-normal">Keep in touch</h5>
+          <p className="text-gray-600">Follow on</p>
         </div>
-        <div className="flex gap-4 flex-wrap">
+        <div className="flex gap-2 flex-wrap">
           <Button
             variant="base"
             className="!p-0 !text-gray-600 hover:!text-gray-900"
+            href="https://www.linkedin.com/in/mohamadalvir"
+            target="_blank"
           >
             Linkedin
           </Button>
           <Button
             variant="base"
             className="!p-0 !text-gray-600 hover:!text-gray-900"
+            href="https://github.com/alvimuh"
+            target="_blank"
+          >
+            Github
+          </Button>
+          <Button
+            variant="base"
+            className="!p-0 !text-gray-600 hover:!text-gray-900"
+            href="https://www.tiktok.com/@alvi.muh"
+            target="_blank"
           >
             Tiktok
           </Button>
           <Button
             variant="base"
             className="!p-0 !text-gray-600 hover:!text-gray-900"
+            href="https://www.instagram.com/alvi.muh"
+            target="_blank"
           >
             Instagram
           </Button>
           <Button
             variant="base"
             className="!p-0 !text-gray-600 hover:!text-gray-900"
+            href="https://twitter.com/alvimuh19"
+            target="_blank"
           >
             Twitter
           </Button>
