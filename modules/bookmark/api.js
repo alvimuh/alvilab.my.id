88744ee4f2c9c06ee69ae42850815279e1b68612
mohamadalvin@gmail.com <alvira@code.id>
2023-06-12 10:00:28 +0700

    Remove clientside sorting

--- modules/bookmark/api.js
+++ modules/bookmark/api.js
@@ -3,12 +3,7 @@ import { alvilabAPI } from "../../utils/axios";
 const fetchBookmark = async () => {
   try {
     const res = await alvilabAPI.get("/bookmark");
-    return res.data.data.sort(function (x, y) {
-      // true values first
-      return x.is_pinned ? 0 : x ? -1 : 1;
-      // false values first
-      // return (x === y)? 0 : x? 1 : -1;
-    });
+    return res.data.data;
   } catch (error) {
     //error handler
     throw new Error("Failed to get bookmark");
